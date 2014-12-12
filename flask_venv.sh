da9039416355e3f4f93aeabcd0779ac8d7c093ce
Aseem Maheshwari <aseemm@gmail.com>
2014-12-12 13:24:06 -0800

    env updates

--- flask_venv.sh
+++ flask_venv.sh
@@ -7,7 +7,13 @@ cd setuptools-6.1/
 python ez_setup.py
 easy_install pip
 cd ../../..
-sudo apt-get install rabbitmq-server
+
+
+#sudo apt-get install -y sqlite3
+#sudo apt-get install mysql-server mysql-client
+#sudo apt-get install python-mysqldb
+#flask/bin/pip install pymysql
+
 flask/bin/pip install flask
 flask/bin/pip install flask-login
 flask/bin/pip install flask-openid
@@ -21,5 +27,7 @@ flask/bin/pip install guess_language
 flask/bin/pip install flipflop
 flask/bin/pip install coverage
 flask/bin/pip install gunicorn
-flask/bin/pip install celery
 flask/bin/pip install requests
+
+#sudo apt-get install rabbitmq-server
+#flask/bin/pip install celery
